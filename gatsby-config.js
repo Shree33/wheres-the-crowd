6a38f8c825a3b72e5b025991da331ca82a313c15
Shreenath Bhanderi <skbhanderi3@gmail.com>
2020-06-14 14:40:10 -0400

    using json to parse vars

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,7 @@ const appConfig = require('./appConfig');
 require("dotenv").config({
   path: `.env`,
 })
-
-//var GSAobj = JSON.parse(process.env.GSA_KEY)
+var GSAobj = JSON.parse(process.env.GSA_KEY)
 //var GSAobj = JSON.parse(GSAstring)
 
 
@@ -22,18 +21,7 @@ module.exports = {
       options: {
         spreadsheetId: SPREADSHEET_ID,
         worksheetTitle: 'Form Responses 1',
-        credentials: {
-          type: "service_account",
-          project_id: "events-279922",
-          private_key_id: process.env.PRIVATE_KEY_ID,
-          private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-          client_email: "eventsprocessor@events-279922.iam.gserviceaccount.com",
-          client_id: '115316605307835110587',
-          auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-          token_uri: 'https://oauth2.googleapis.com/token',
-          auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-          client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/eventsprocessor%40events-279922.iam.gserviceaccount.com",
-        }
+        credentials: GSAobj,
       },
     },
     {
