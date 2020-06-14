0cc86ff73771b676e15c0dbb7839e44c694e16b7
Shreenath Bhanderi <skbhanderi3@gmail.com>
2020-06-14 14:31:28 -0400

    removed unnecessary function

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,18 +6,6 @@ require("dotenv").config({
 //var GSAobj = JSON.parse(process.env.GSA_KEY)
 //var GSAobj = JSON.parse(GSAstring)
 
-const buildCredentials = ({ PRIVATE_KEY, PRIVATE_KEY_ID }) => ({
-  type: "service_account",
-  project_id: "events-279922",
-  private_key_id: PRIVATE_KEY_ID,
-  private_key: PRIVATE_KEY.replace(/\\n/g, '\n'),
-  client_email: "eventsprocessor@events-279922.iam.gserviceaccount.com",
-  client_id: '115316605307835110587',
-  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-  token_uri: 'https://oauth2.googleapis.com/token',
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/eventsprocessor%40events-279922.iam.gserviceaccount.com",
-});
 
 const SPREADSHEET_ID = '199tGxqYHxHclVMH7bailF69PoxrP32GGapC-I4ivaBo';
 
@@ -34,7 +22,18 @@ module.exports = {
       options: {
         spreadsheetId: SPREADSHEET_ID,
         worksheetTitle: 'Form Responses 1',
-        credentials: buildCredentials(process.env)
+        credentials: {
+          type: "service_account",
+          project_id: "events-279922",
+          private_key_id: process.env.PRIVATE_KEY_ID,
+          private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+          client_email: "eventsprocessor@events-279922.iam.gserviceaccount.com",
+          client_id: '115316605307835110587',
+          auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+          token_uri: 'https://oauth2.googleapis.com/token',
+          auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+          client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/eventsprocessor%40events-279922.iam.gserviceaccount.com",
+        }
       },
     },
     {
