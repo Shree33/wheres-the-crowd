00bdef0a23b6f1be71835f75d463251053af113a
Shreenath Bhanderi <skbhanderi3@gmail.com>
2020-06-26 19:43:28 -0400

    changed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Box, Text, Anchor, Form, Button, TextInput, FormField } from 'grommet';
 
 
 const Footer = () => (
-  <Box tag="footer" justify="between" direction="row" pad="medium">
+  <Box tag="footer" justify="between" direction="row" pad="medium" flex="true">
     <Form direction="row" action="https://getform.io/f/b61c1bc5-c567-4f7e-ace8-3e55883a188d" method="POST">
       <Box direction="row" >
         <FormField name ="instructions" label="Want events sent to your mailbox? Put in your email here!"> 
@@ -11,14 +11,15 @@ const Footer = () => (
         </FormField>
         <Button type="submit" label="send" alignSelf="end" secondary gap="xsmall" margin="xsmall"></Button>
       </Box>
-    </Form>
-
-    <Text>
+      
+    <Text alignSelf="end">
       Questions? Contact us at: 
       <Anchor href="mailto:info@wheresthecrowd.com">
         <b>info@wheresthecrowd.com</b>
       </Anchor>
     </Text>
+    </Form>
+
   </Box>
 );
 
